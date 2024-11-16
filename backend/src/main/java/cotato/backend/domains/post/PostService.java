7ac228db9dab623364da64addce725c86aef9caf
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-11-16 23:35:24 +0900

    refactor: PostService Transactional 범위 변경 및 toList 단순화

--- backend/src/main/java/cotato/backend/domains/post/PostService.java
+++ backend/src/main/java/cotato/backend/domains/post/PostService.java
@@ -17,10 +17,11 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Service
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
-@Transactional
+@Transactional(readOnly = true)
 public class PostService {
 
 	// 로컬 파일 경로로부터 엑셀 파일을 읽어 Post 엔터티로 변환하고 저장
+	@Transactional
 	public void saveEstatesByExcel(String filePath) {
 		try {
 			// 엑셀 파일을 읽어 데이터 프레임 형태로 변환
@@ -30,9 +31,14 @@ public class PostService {
 					String content = row.get("content");
 					String name = row.get("name");
 
-					return new Post(title, content, name);
+					Post post = Post.builder()
+						.title(title)
+						.content(content)
+						.name(name)
+						.build();
+					return post;
 				})
-				.collect(Collectors.toList());
+				.toList();
 
 		} catch (Exception e) {
 			log.error("Failed to save estates by excel", e);

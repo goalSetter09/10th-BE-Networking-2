56d3303a309726535051c13cec3614edc5a48fa1
Dongyoung Kim <145846246+goalSetter09@users.noreply.github.com>
2024-12-26 02:01:43 +0900

    refactor: Post에 대한 개념 객체 생성

--- backend/src/main/java/cotato/backend/domains/post/dto/PostConcept.java
+++ backend/src/main/java/cotato/backend/domains/post/dto/PostConcept.java
@@ -1,20 +1,22 @@
-package cotato.backend.domains.post.dto.response;
+package cotato.backend.domains.post.dto;
 
 import cotato.backend.domains.post.Post;
 
-public record SinglePostFindResponse(
+public record PostConcept(
+	Long id,
 	String title,
 	String content,
 	String author,
 	Long views
 ) {
-
-	public static SinglePostFindResponse from(Post post) {
-		return new SinglePostFindResponse(
+	public static PostConcept from(Post post) {
+		return new PostConcept(
+			post.getId(),
 			post.getTitle(),
 			post.getContent(),
 			post.getName(),
 			post.getViews()
 		);
 	}
+
 }
